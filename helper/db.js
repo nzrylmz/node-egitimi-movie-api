a4a9bed3604ef3da99219275aa6f53615fe5b100
nzrylmz <nzrylmz05@gmail.com>
2019-03-09 17:38:54 +0300

    heroku deploy

--- helper/db.js
+++ helper/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb://movie_user:abcd1234@ds261155.mlab.com:61155/heroku_fg7l8395', {useNewUrlParser:true, useCreateIndex:true});
+    mongoose.connect('mongodb://heroku_q9dfm6jn:heroku_q9dfm6jn@ds117681.mlab.com:17681/heroku_q9dfm6jn', {useNewUrlParser:true, useCreateIndex:true});
     mongoose.connection.on('open', () => {
-        //console.log('MongoDB: Connected');
+        console.log('MongoDB: Connected');
     }).on('error', (err) => {
         console.log('MongoDB: Error', err);
     });
